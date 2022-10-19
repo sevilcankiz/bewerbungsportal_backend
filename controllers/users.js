f63366bb05c5e5e320f681a270c05fc4ed801886
Rauolli <rauol@t-online.de>
2022-10-19 20:25:14 +0200

    MongoDB user-documents CRUD-functions ready to use

--- controllers/users.js
+++ controllers/users.js
@@ -81,8 +81,8 @@ const updateUser = async(req, res) => {
                 phone
             },
             certificates: [{
-                certName,
-                certURL
+                cerName,
+                cerURL
             }],
             resumes: [{ 
                 resName,
@@ -101,7 +101,7 @@ const updateUser = async(req, res) => {
                 email,
                 phone: [phone]
             },
-            certificates: [{certName, certURL}],
+            certificates: [{cerName, cerURL}],
             resumes: [{resName, resURL}]
         }
     );
@@ -123,12 +123,110 @@ const deleteUser = async(req, res) => {
     }
 };
 
+// ###### CRUD-Methoden for User-Documents of Type: ( cerificates / Resumes) ######
 
+// GET /users/:id/:type/  (alle Dokumente eines Typs) OKAY!!  
+const getAllUserDocumentsofType = async(req, res) => {
+    try {
+        const { id, type } = req.params;
+        type !== 'resumes' && type !== 'certificates' ? res.status(404).send('Not Found') : null;
+        const user = await User.findById(id);
+        const documents = user[type];
+        res.status(200).json(documents);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+// POST /users/:id/:type/ (einen neuen Dokumenttyp hinzufügen) OKAY!!
+const createDocumentofType = async(req, res) => {
+    try {
+        const { id, type } = req.params;
+        type !== 'resumes' && type !== 'certificates' ? res.status(404).send('Not Found') : null;
+        if (type === 'resumes') {
+            const { resName, resURL } = req.body;
+            const user = await User.findById(id);
+            user.resumes.push({ resName, resURL });
+            await user.save();
+            res.status(200).json(user.resumes);            
+        } else {
+            const { certName, certURL } = req.body;
+            const user = await User.findById(id);
+            user.certificates.push({ certName, certURL });
+            await user.save();
+            res.status(200).json(user.certificates);           
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+
+// GET /users/:id/:type/:docId (ein Dokument eines Typs) OKAY!!
+const getSingleUserDocumentofType = async(req, res) => {
+    try {
+        const { id, type, docId } = req.params;
+        type !== 'resumes' && type !== 'certificates' ? res.status(404).send('Not Found') : null;
+        const user = await User.findById(id);
+        const document = user[type].id(docId);
+        res.status(200).json(document);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+// PUT /users/:id/:type/:docId (ein Dokument eines Typs updaten) OKAY!!
+const updateDocumentofType = async(req, res) => {
+    try {
+        const { id, type, docId } = req.params;
+        console.log(id, type, docId);
+        type !== 'resumes' && type !== 'certificates' ? res.status(404).send('Not Found') : null;
+        if (type === 'resumes') {
+            const { resName, resURL } = req.body;
+            const user = await User.findById(id);
+            const document = user[type].id(docId);
+            document.resName = resName;
+            document.resURL = resURL;
+            await user.save();
+            res.status(200).json(document);            
+        } else {
+            const { certName, certURL } = req.body;
+            const user = await User.findById(id);
+            const document = user[type].id(docId);
+            document.certName = certName;
+            document.certURL = certURL;
+            await user.save();
+            res.status(200).json(document);           
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+// DELETE /users/:id/:type/:docId (ein Dokument eines Typs) OKAY!!
+const deleteDocumentofType = async(req, res) => {
+    try {
+        const { id, type, docId } = req.params;
+        type !== 'resumes' && type !== 'certificates' ? res.status(404).send('Not Found') : null;
+        const user = await User.findById(id);
+        const document = user[type].id(docId);
+        document.remove();
+        await user.save();
+        res.status(200).json(user[type]);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
 
 module.exports={
-                getAllUsers, 
-                getSingleUser,
-                createUser,
-                updateUser,
-                deleteUser,
-            };
+    getAllUsers, 
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    getAllUserDocumentsofType,
+    getSingleUserDocumentofType,
+    createDocumentofType,
+    updateDocumentofType,
+    deleteDocumentofType
+};
--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,11 @@ const {
     createUser, 
     updateUser, 
     deleteUser,
+    getAllUserDocumentsofType,
+    getSingleUserDocumentofType,
+    createDocumentofType,
+    updateDocumentofType,
+    deleteDocumentofType
     } = require('../controllers/users');
 
 const usersRouter = express.Router();
@@ -13,5 +18,9 @@ const usersRouter = express.Router();
 // Users
 usersRouter.route('/').get(getAllUsers).post(createUser);
 usersRouter.route('/:id').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// User-Documents of Type: ( Certificates / Resumes)
+usersRouter.route('/:id/:type').get(getAllUserDocumentsofType).post(createDocumentofType);
+usersRouter.route('/:id/:type/:docId').get(getSingleUserDocumentofType).put(updateDocumentofType).delete(deleteDocumentofType);
 
 module.exports=usersRouter;
