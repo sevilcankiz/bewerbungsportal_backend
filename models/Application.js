9f7c43e13e3bb97ddc9fbc5639f15856272ed863
Rauolli <rauol@t-online.de>
2022-10-20 11:55:47 +0200

    added CRUD-functions for interacting MongoDB with applications

--- models/Application.js
+++ models/Application.js
@@ -3,13 +3,11 @@ const { Schema } = mongoose;
 
 const Application = new Schema({
         userID: Object,
-        jobTitle: String,
-        
+        jobTitle: String,       
         company: {
                 companyName: String,
                 contactPerson: { 
-                        firstName: String,
-                        lastName: String,
+                        fullName: String,
                         gender: String
                 },
                 address: {
@@ -17,25 +15,17 @@ const Application = new Schema({
                         street: String,
                         city: String,
                         email: String,
-                        phone: [ String ]
+                        phone: String,
+                        website: String
                 }
         },
-        resume: {
-                name: String,
-                url: String
-        },
         motivation: {
-                name: String,
-                url: String
+                motName: String,
+                motURL: String
         },
-        certificates: [
-                {
-                name: String,
-                url: String
-        }],
-        
-        date: Date
-        
+        resumeID: Object,
+        certificates: [ {certID: Object} ],
+        date: {type: Date, default: Date.now}       
 });
 
 module.exports = mongoose.model('applications', Application);
--- routes/applications.js
+++ routes/applications.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
 //Controllers
-const getAllApplications = require('../controllers/applications');
+const { 
+    getAllApplications,
+    getSingleApplication,
+    createApplication, 
+    deleteApplication,
+} = require('../controllers/applications');
 
 const applicationsRouter = express.Router();
-applicationsRouter.route("/").get(getAllApplications);
-
-
-
+applicationsRouter.route("/").get(getAllApplications).post(createApplication);
+applicationsRouter.route("/:id").get(getSingleApplication).delete(deleteApplication);
 
 module.exports=applicationsRouter;
